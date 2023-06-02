d10c955f0435a239a1425aa7b71b79ccb47ae045
jfortkamp8 <135294542+jfortkamp8@users.noreply.github.com>
2023-06-02 00:31:28 -0400

    Update create_donation_dashboard.php

--- create_donation_dashboard.php
+++ create_donation_dashboard.php
@@ -1,79 +1,84 @@
 function createDonationDashboard($goal) {
 
-    $formattedGoal = '$'.number_format($goal);
-    
+    $formattedGoal = '$' . number_format($goal);
+
     $totalDonations = 500000;
-    $formattedTotalDonations = '$'.number_format($totalDonations);
-    $pendingAmount = $totalDonations - $goal;
-    $formattedPendingAmount = '$'.number_format($pendingAmount);
     $percent = $totalDonations / $goal * 100;
     $meterFillStyle = "width: " . $percent . "%";
     $meterFillContent = '<div class="fill" style="width: ' . $percent . '%">' . ($percent > 100 ? '<p>' . round($percent) . '%</p>' : '') . '</div>';
-    
+
+
+    $pledgedAmount = '$' . number_format($totalDonations);
+    $pendingAmount = '$123,456'; // Replace with your pending amount variable
+
     // HTML code for the donation dashboard
     $dashboard = '
-        <div style="display: flex; flex-direction: column; background-color: #FFFFFF; border-radius: 10px; padding: 20px;">
-            <div style="width: 100%; height: 200px; background-color: #7866A1; border-radius: 10px; margin-bottom: 20px;">
+        <div style="display: flex; flex-direction: column; background-color: #F0F0F0; border-radius: 10px; padding: 20px; box-shadow: 0 3px 6px rgba(0, 0, 0, 0.3); margin-bottom: 20px;">
+            <div style="width: 100%; height: 140px; background-color: #77C4D5; border-radius: 10px; margin-bottom: 28px; display: flex; justify-content: center; align-items: center;">
                 <!-- Campaign goal box -->
-                <div style="text-align: center; padding-top: 50px;">
-                    <h2 style="color: #FFFFFF;">' .$formattedGoal. ' Goal</h2>
-                </div>
+                <h2 style="color: #FFFFFF; font-size: 52px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3); padding: 0 20px;">' . $formattedGoal . ' Campaign Goal</h2>
             </div>
-            <div style="width: 100%; height: 50px; background-color: #77C4D5; border-radius: 10px; margin-bottom: 20px;">
+            <div style="width: 100%; height: 90px; background-color: #7866A1; border-radius: 10px; margin-bottom: 28px; display: flex; justify-content: center; align-items: center; padding: 10px;">
                 <!-- Donation meter box -->
-                <div style="text-align: center; padding-top: 10px;">
-                    <h3 style="color: #FFFFFF;">Donation Meter</h3>
-                    <div class="dashboard-meter">
-                        ' . $meterFillContent . '
-                    </div>
+                <div class="dashboard-meter">
+                    ' . $meterFillContent . '
                 </div>
             </div>
-            <div style="display: flex; flex-direction: row; width: 100%; margin-bottom: 20px;">
-                <div style="width: 50%; background-color: #00758D; border-radius: 10px;">
-                    <!-- Amount pledged box -->
-                    <div style="text-align: center;">
-                        <h3 style="color: #FFFFFF;">Amount Pledged</h3>
-                        <p style="color: #FFFFFF; font-weight: bold; font-size: 20px;">' .$formattedTotalDonations. ' Pledged</p>
-                    </div>
-                </div>
-                <div style="width: 50%; background-color: #F78D2D; border-radius: 10px;">
-                    <!-- Amount pending box -->
-                    <div style="text-align: center;">
-                        <h3 style="color: #FFFFFF;">Amount Pending</h3>
-                        <p style="color: #FFFFFF; font-weight: bold; font-size: 20px;">' .$formattedPendingAmount. ' Pending</p>
-                    </div>
-                </div>
-            </div>
-            <div style="width: 100%; height: 200px; background-color: #77C4D5; border-radius: 10px;">
-                <!-- Bar graph chart -->
-                <h3 style="color: #FFFFFF;">Bar Graph Chart</h3>
-                <!-- Add bar graph chart here -->
+            <div style="display: flex; justify-content: space-between; border-radius: 10px; margin-bottom: 28px; align-items: center;">
+        <div style="width: 49%; background-color: #00758D; height: 80px; border-radius: 10px; display: flex; justify-content: center; align-items: center; padding: 10px; box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);">
+            <!-- Amount pledged box -->
+            <div>
+                <h3 style="color: #FFFFFF; font-size: 36px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">' . $pledgedAmount . '  Pledged</h3>
             </div>
         </div>
-        
+        <div style="width: 49%; background-color: #F78D2D; height: 80px; border-radius: 10px; display: flex;  justify-content: center; align-items: center; padding: 10px; box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);">
+            <!-- Amount pending box -->
+            <div>
+                <h3 style="color: #FFFFFF; font-size: 36px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">' . $pendingAmount . '  Pending</h3>
+            </div>
+        </div>
+    </div>
+            <div style="width: 100%; height: 200px; background-color: #77C4D5; border-radius: 10px; display: flex; justify-content: center; align-items: center;">
+                <!-- Bar graph chart -->
+                <h3 style="color: #FFFFFF; font-size: 36px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">Bar Graph Chart</h3>
+            </div>
+        </div>
+
         <style>
             .dashboard-meter {
-                width: 100%;
-                height: 20px;
-                background: #F78D2D;
+                width: 97%;
+                height: 52px;
+                background: #FFFFFF;
                 position: relative;
-                border-radius: 13px;
                 overflow: hidden;
+                border-radius: 10px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding: 10px;
+                box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
             }
 
             .dashboard-meter .fill {
                 transition: width 0.5s ease-out;
-                border-radius: 13px;
-                position: inherit;
-                background: #F78D2D;
-                width: 100%;
+                position: absolute;
+                top: 0;
+                left: 0;
                 height: 100%;
+                background: linear-gradient(to right,#F78D2D,#FFC897);
                 display: flex;
                 align-items: center;
                 justify-content: flex-end;
                 padding-right: 10px;
                 box-sizing: border-box;
                 animation: fillAnimation 2s ease-in-out;
+            }
+
+            .dashboard-meter .fill p {
+                color: #FFFFFF;
+                font-size: 18px;
+                font-weight: bold;
+                margin-right: 10px;
             }
 
             @keyframes fillAnimation {
