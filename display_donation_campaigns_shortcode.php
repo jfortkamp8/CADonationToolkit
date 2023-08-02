c192dd3fee124b5fd11c6842c345d4caf2e23fc3
jfortkamp8 <135294542+jfortkamp8@users.noreply.github.com>
2023-08-02 11:50:54 -0400

    Update display_donation_campaigns_shortcode.php

--- display_donation_campaigns_shortcode.php
+++ display_donation_campaigns_shortcode.php
@@ -1,13 +1,12 @@
+
 function display_donation_campaigns_shortcode($atts) {
 	$is_admin = current_user_can('manage_options');
 	
-    // Set the query arguments
     $args = array(
         'post_type' => 'Campaigns',
         'posts_per_page' => -1,
     );
 
-    // If the user is not an admin, filter campaigns by the current user's ID or assigned user ACF field value
     if (!$is_admin) {
         $user_id = get_current_user_id();
         $args['meta_query'] = array(
@@ -24,19 +23,15 @@ function display_donation_campaigns_shortcode($atts) {
             ),
         );
     } else {
-        // If the user is an admin, remove the meta_query
         $args['meta_query'] = false;
     }
 
-    // Run the query
     $campaigns_query = new WP_Query($args);
 
-    // Start the output buffer
     ob_start();
 
-    // Add the CSS styles
     echo '<style>
-.donation-campaigns-grid {
+        .donation-campaigns-grid {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -124,17 +119,55 @@ function display_donation_campaigns_shortcode($atts) {
   transition: background-color 0.3s ease-in-out;
 }
 
+
+    /* Additional CSS for new buttons */
+    .campaign-edit-button,
+    .campaign-delete-button {
+        position: absolute;
+        top: 8px;
+        display: inline-block;
+        background-color: #00758d;
+        border-radius: 50%;
+        color: #fff;
+        text-align: center;
+        width: 32px;
+        height: 32px;
+        line-height: 30px;
+    }
+
+    .campaign-edit-button {
+        right: 45px;
+    }
+
+    .campaign-delete-button {
+        right: 10px;
+    }
+
+    .add-campaign-button {
+        position: absolute;
+        top: -88px;
+        right: 153px;
+        background-color: #D8D8D8;
+        border-radius: 50%;
+        color: #fff;
+		vertical-align: middle;
+		padding: 6px;
+        text-align: center;
+		font-size: 40px;
+        width: 50px;
+        height: 50px;
+        line-height: 30px;
+    }
+
     </style>';
 
-    // Display the campaigns
     if ($campaigns_query->have_posts()) {
-        echo '<div class="row donation-campaigns-grid">'; // Update the div class according to your theme's grid system
+        echo '<div class="row donation-campaigns-grid">';
 
         while ($campaigns_query->have_posts()) {
             $campaigns_query->the_post();
 
-            
-           
+                       
 // Get the featured image
 $image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')[0];
 
@@ -151,12 +184,24 @@ echo '<div class="col-md-4 col-sm-6 donation-campaign-box">';
                 echo '<div class="campaign-image">';
                     echo '<img src="' . $image_url . '" alt="' . get_the_title() . '">';
                     echo '<div class="campaign-goal">' . get_field('donation_goal') . ' Goal</div>';
+			echo '<div class="campaign-button-container">';
+
+if (current_user_can('edit_post', get_the_ID())) {
+    echo '<a href="' . admin_url('post.php?post=' . get_the_ID() . '&action=edit') . '" target="_blank" class="campaign-edit-button" style="display: flex; justify-content: center; align-items: center;"><img src="https://www.freeiconspng.com/uploads/edit-editor-pen-pencil-write-icon--4.png" style="width: 50%; height: 50%; object-fit: cover; filter: invert(100%);"></a>';
+}
+
+if (current_user_can('delete_post', get_the_ID())) {
+    echo '<a href="' . get_delete_post_link(get_the_ID()) . '" class="campaign-delete-button" onclick="return confirm(\'Are you sure you want to delete this campaign?\')" style="display: flex; justify-content: center; align-items: center;"><img src="https://www.pinclipart.com/picdir/big/159-1597907_delete-garbage-remove-trash-trash-can-icon-delete.png" style="width: 50%; height: 50%; object-fit: cover; filter: invert(100%);"></a>';
+}
+
+
+            echo '</div>';
                 echo '</div>';
                 echo '<div class="campaign-title">' . get_field('campaign_name') . '</div>';
                 echo '<div class="campaign-info">';
                     echo '<ul>';
                         echo '<li>' . get_field('campaign_description') . '</li>';
-                        //echo '<li>Location: ' . get_field('campaign_location') . '</li>';
+                        echo '<li>Location: ' . get_field('location') . '</li>';
                     echo '<ul>';
                 echo '</div>';
                 $campaign_id = get_the_ID();
@@ -186,16 +231,23 @@ echo '<div class="col-md-4 col-sm-6 donation-campaign-box">';
                     echo '<a href="' . $campaign_button_url . '" style="font-size: 13px; font-style: bold;" class="campaign-button">Launch Campaign Toolkit</a>';
                 echo '</div>';
             echo '</div>';
+
         }
+
         echo '</div>';
+
+        if (current_user_can('publish_posts')) {
+echo '<a href="' . admin_url('post-new.php?post_type=campaigns') . '" target="_blank" class="add-campaign-button" style="display: inline-block; vertical-align: middle;">+</a>';
+
+
+
+        }
+
     } else {
         echo '<p>No donation campaigns found.</p>';
     }
 
-    // Clean up after the query
     wp_reset_postdata();
-
-    // Get the output buffer contents and stop buffering
     $output = ob_get_clean();
 
     return $output;
