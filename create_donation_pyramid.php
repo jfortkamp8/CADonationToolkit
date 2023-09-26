acdaf196691cafee00bc56c0a00d5b11d513eac3
jfortkamp8 <135294542+jfortkamp8@users.noreply.github.com>
2023-09-26 12:37:14 -0400

    Update create_donation_pyramid.php

--- create_donation_pyramid.php
+++ create_donation_pyramid.php
@@ -1,63 +1,78 @@
 function createDonationPyramid($goal) {
-  $row_amounts = array(5000, 10000, 25000, 50000, 75000, 100000, 250000, 500000, 1250000, 2500000);
-  rsort($row_amounts);
+$row_amounts = array(5000, 10000, 25000, 50000, 75000, 100000, 250000, 500000, 1000000, 1500000, 2000000, 3000000, 5000000);
+rsort($row_amounts);
 
-  $unadjusted_goal = $goal;
+$rows = [];
+$totalDonations = 0;
 
-  if ($goal <= 1000000) {
-    $max_boxes = array(
-      5000 => 8,
-      10000 => 8,
-      25000 => 6,
-      50000 => 4,
-      75000 => 2,
-      100000 => 1,
-      250000 => 1,
-    );
-    $rowCount = count($row_amounts) - 4;
-  } elseif ($goal <= 2000000 && $goal > 1000000) {
-    $max_boxes = array(
-      5000 => 8,
-      10000 => 8,
-      25000 => 7,
-      50000 => 6,
-      75000 => 4,
-      100000 => 3,
-      250000 => 1,
-      500000 => 1,
-    );
-    $rowCount = count($row_amounts) - 3;
-  } else {
-    $max_boxes = array(
-      10000 => 8,
-      25000 => 8,
-      50000 => 7,
-      75000 => 7,
-      100000 => 6,
-      250000 => 4,
-      500000 => 2,
-      1250000 => 1,
-    );
-    $rowCount = count($row_amounts) - 3;
-  }
-  
-  $totalDonations = $unadjusted_goal - $goal; // Initialize the total donations
-	
-  $rows = array();
-  foreach ($row_amounts as $amount) {
-    if (isset($max_boxes[$amount])) {
-      $num_boxes = min(floor($goal / $amount), $max_boxes[$amount]);
-      if ($num_boxes > 0) {
-        $goal -= $num_boxes * $amount;
-         array_push($rows, array('id' => 'row' . ($rowCount--), 'num_boxes' => $num_boxes, 'amount' => $amount));
-      $totalDonations += $num_boxes * $amount; // Update the total donations
-    }
+// Top box is 30% of the goal
+$top_box_amount = $goal * 0.3;
 
-      if ($goal == 0) {
+foreach ($row_amounts as $amount) {
+    if ($amount <= $top_box_amount) {
+        $top_box_amount = $amount;
         break;
-      }
     }
-  }
+}
+
+array_push($rows, array('id' => 'row' . ($top_box_amount / 1000), 'num_boxes' => 1, 'amount' => $top_box_amount));
+$goal -= $top_box_amount;
+$totalDonations += $top_box_amount;
+
+$boxes = 2;  
+$remaining_goal_percentages = [0.35, 0.3]; 
+$current_goal_percentage_index = 0;
+
+foreach ($row_amounts as $key => $amount) {
+    if ($amount >= $top_box_amount) {
+        unset($row_amounts[$key]);
+    }
+}
+rsort($row_amounts);
+
+foreach ($row_amounts as $amount) {
+    if ($goal <= 0 || count($rows) >= 8) break;
+    
+    $target_for_this_row = $goal * $remaining_goal_percentages[$current_goal_percentage_index];
+    $num_boxes = min(floor($target_for_this_row / $amount), $boxes);
+    
+    if ($num_boxes > 0) {
+        while ($num_boxes > 8) {
+            array_push($rows, array('id' => 'row' . ($amount / 1000), 'num_boxes' => 8, 'amount' => $amount));
+            $goal -= 8 * $amount;
+            $totalDonations += 8 * $amount;
+            $num_boxes -= 8;
+        }
+
+        $goal -= $num_boxes * $amount;
+        array_push($rows, array('id' => 'row' . ($amount / 1000), 'num_boxes' => $num_boxes, 'amount' => $amount));
+        $totalDonations += $num_boxes * $amount;
+
+        $boxes = $num_boxes + rand(1, 3);  
+
+        if (count($rows) == 4) {
+            $current_goal_percentage_index = 1;
+        }
+    }
+}
+
+// Go back and fill in more boxes to meet the goal
+foreach (array_reverse($rows) as $index => $row) {
+    while ($goal > 0 && $rows[count($rows) - 1 - $index]['num_boxes'] < 8) {
+        if ($goal >= $row['amount']) {
+            $goal -= $row['amount'];
+            $totalDonations += $row['amount'];
+            $rows[count($rows) - 1 - $index]['num_boxes'] += 1;
+        } else {
+            break;
+        }
+    }
+}
+
+// Ensure at least 7 rows
+while (count($rows) < 7) {
+    array_push($rows, array('id' => 'row' . ($row_amounts[count($row_amounts) - 1] / 1000), 'num_boxes' => 1, 'amount' => $row_amounts[count($row_amounts) - 1]));
+}
 
   echo '<div class="donation-pyramid">';
   echo '<div class="donation-key">';
